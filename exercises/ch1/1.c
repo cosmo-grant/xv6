7c76ee6fde013ad6cd762f689b6b14ad096a6236
Cosmo Grant <cosmodgrant@gmail.com>
2026-09-22 17:05:20 -0400

    calculate exchange rate

--- exercises/ch1/1.c
+++ exercises/ch1/1.c
@@ -4,13 +4,22 @@
 // - parent writes/reads
 // - child reads/writes
 
+#include <bits/time.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
+volatile int keep_running = 1;
+
+void interruptHandler(int sig) { keep_running = 0; }
+
 int main(int argc, char *argv[]) {
+  signal(SIGINT, interruptHandler);
+
   int ping[2];
   int pong[2];
   pipe(ping);
@@ -23,21 +32,29 @@ int main(int argc, char *argv[]) {
   }
   char buf = argv[1][0];
 
+  int exchanges = 0;
+
   int pid = fork();
   if (pid == 0) {
-    while (1) {
+    while (keep_running) {
       read(ping[0], &buf, 1);
-      sleep(1);
       write(pong[1], &buf, 1);
-      printf("pong: %c\n", buf);
     }
   } else if (pid > 0) {
-    while (1) {
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    while (keep_running) {
       write(ping[1], &buf, 1);
-      printf("ping: %c\n", buf);
       read(pong[0], &buf, 1);
-      sleep(1);
+      exchanges++;
     }
+
+    // exchange rate
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double elapsed_seconds = (end.tv_sec - start.tv_sec) +
+                             (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    printf("exchanges per second: %f\n", exchanges / elapsed_seconds);
+
   } else {
     printf("fork error\n");
   }
