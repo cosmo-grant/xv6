95f9f6b96cd9927537e3f738cbcf01fbd1635c03
Cosmo Grant <cosmodgrant@gmail.com>
2026-09-22 13:17:39 -0400

    never-ending rally

--- exercises/ch1/1.c
+++ exercises/ch1/1.c
@@ -12,22 +12,26 @@
 int main(int argc, char *argv[]) {
   int ping[2];
   int pong[2];
-  pipe(ping); // create a pipe; put read/write file descriptors in ping
+  pipe(ping);
   pipe(pong);
 
   char buf[1] = "x";
 
   int pid = fork();
   if (pid == 0) {
-    read(ping[0], buf, 1);
-    sleep(1);
-    write(pong[1], buf, 1);
-    printf("pong\n");
+    while (1) {
+      read(ping[0], buf, 1);
+      sleep(1);
+      write(pong[1], buf, 1);
+      printf("pong\n");
+    }
   } else if (pid > 0) {
-    write(ping[1], buf, 1);
-    printf("ping\n");
-    read(pong[0], buf, 1);
-    sleep(1);
+    while (1) {
+      write(ping[1], buf, 1);
+      printf("ping\n");
+      read(pong[0], buf, 1);
+      sleep(1);
+    }
   } else {
     printf("fork error\n");
   }
