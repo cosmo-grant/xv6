78e54207d75aeb87e9edbe39f063f0691c323c40
Cosmo Grant <cosmodgrant@gmail.com>
2026-09-22 13:06:43 -0400

    ping pong

---
+++ exercises/ch1/1.c
@@ -0,0 +1,34 @@
+// ping pong byte over pair of processes
+// - set up pipes
+// - fork
+// - parent writes/reads
+// - child reads/writes
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+  int ping[2];
+  int pong[2];
+  pipe(ping); // create a pipe; put read/write file descriptors in ping
+  pipe(pong);
+
+  char buf[1] = "x";
+
+  int pid = fork();
+  if (pid == 0) {
+    read(ping[0], buf, 1);
+    sleep(1);
+    write(pong[1], buf, 1);
+    printf("pong\n");
+  } else if (pid > 0) {
+    write(ping[1], buf, 1);
+    printf("ping\n");
+    read(pong[0], buf, 1);
+    sleep(1);
+  } else {
+    printf("fork error\n");
+  }
+}
