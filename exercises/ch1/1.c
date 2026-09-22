9efd79f6a866df8b619983726f99fdc1f4d3d9b5
Cosmo Grant <cosmodgrant@gmail.com>
2026-09-22 13:30:32 -0400

    rally any char

--- exercises/ch1/1.c
+++ exercises/ch1/1.c
@@ -5,9 +5,10 @@
 // - child reads/writes
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
   int ping[2];
@@ -15,21 +16,26 @@ int main(int argc, char *argv[]) {
   pipe(ping);
   pipe(pong);
 
-  char buf[1] = "x";
+  if (argc < 2 || strlen(argv[1]) != 1) {
+    printf("Expected 1 byte. Got %s, which is %zu bytes.\n", argv[1],
+           sizeof argv[1]);
+    exit(1);
+  }
+  char buf = argv[1][0];
 
   int pid = fork();
   if (pid == 0) {
     while (1) {
-      read(ping[0], buf, 1);
+      read(ping[0], &buf, 1);
       sleep(1);
-      write(pong[1], buf, 1);
-      printf("pong\n");
+      write(pong[1], &buf, 1);
+      printf("pong: %c\n", buf);
     }
   } else if (pid > 0) {
     while (1) {
-      write(ping[1], buf, 1);
-      printf("ping\n");
-      read(pong[0], buf, 1);
+      write(ping[1], &buf, 1);
+      printf("ping: %c\n", buf);
+      read(pong[0], &buf, 1);
       sleep(1);
     }
   } else {
